8cf8280d63391c683c4cd496651fac986500eaf7
Ptunda <ptunda@pgh.yearup.org>
2024-05-12 18:31:47 -0400

    Completed phase 1

---
+++ src/main/java/com/pluralsight/DealershipFileManager.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+public class DealershipFileManager {
+
+    public Dealership getDealership() {
+        return null;
+    }
+
+    public void saveDealership(Dealership dealership) {
+
+    }
+
+}
---
+++ src/main/java/com/pluralsight/UserInterface.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+
+public class UserInterface {
+
+    Dealership dealership;
+
+    public UserInterface() {
+        this.dealership = new Dealership("Car World", "123 Main St", "555-123-4567");
+    }
+
+    public void display() {
+
+    }
+
+    public void processGetByPriceRequest() {
+
+    }
+
+    public void processGetByMakeModelRequest() {
+
+    }
+
+    public void processGetByYearRequest() {
+
+    }
+
+    public void processGetByColorRequest() {
+
+    }
+
+    public void processGetByMileageRequest() {
+
+    }
+
+    public void processGetByVehicleTypeRequest() {
+
+    }
+
+    public void processGetAllVehiclesRequest() {
+
+    }
+
+    public void processAddVehicleRequest() {
+
+    }
+
+    public void processRemoveVehicleRequest() {
+
+    }
+
+
+}
